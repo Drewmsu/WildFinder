a8443e7b38f7c2d8b05d1ff64a2a8a9279e471d2
Drewmsu <fran.mp07@gmail.com>
2017-11-03 00:26:32 -0500

    Basic functionality fixed

---
+++ WildFinder/WildFinder/AnimalsForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WildFinder
+{
+    partial class AnimalsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbHabitat = new System.Windows.Forms.CheckBox();
+            this.btnConsult = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lbConsult = new System.Windows.Forms.ListBox();
+            this.cb3 = new System.Windows.Forms.CheckBox();
+            this.cb2 = new System.Windows.Forms.CheckBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbHabitat
+            // 
+            this.cbHabitat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbHabitat.AutoSize = true;
+            this.cbHabitat.Location = new System.Drawing.Point(23, 82);
+            this.cbHabitat.Name = "cbHabitat";
+            this.cbHabitat.Size = new System.Drawing.Size(60, 17);
+            this.cbHabitat.TabIndex = 56;
+            this.cbHabitat.Text = "Habitat";
+            this.cbHabitat.UseVisualStyleBackColor = true;
+            // 
+            // btnConsult
+            // 
+            this.btnConsult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnConsult.Location = new System.Drawing.Point(22, 372);
+            this.btnConsult.Name = "btnConsult";
+            this.btnConsult.Size = new System.Drawing.Size(109, 36);
+            this.btnConsult.TabIndex = 55;
+            this.btnConsult.Text = "Consult";
+            this.btnConsult.UseVisualStyleBackColor = true;
+            this.btnConsult.Click += new System.EventHandler(this.btnConsult_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput.Location = new System.Drawing.Point(23, 129);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(151, 20);
+            this.tbInput.TabIndex = 54;
+            // 
+            // lbConsult
+            // 
+            this.lbConsult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbConsult.FormattingEnabled = true;
+            this.lbConsult.Location = new System.Drawing.Point(22, 167);
+            this.lbConsult.Name = "lbConsult";
+            this.lbConsult.Size = new System.Drawing.Size(515, 199);
+            this.lbConsult.TabIndex = 53;
+            // 
+            // cb3
+            // 
+            this.cb3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb3.AutoSize = true;
+            this.cb3.Location = new System.Drawing.Point(260, 82);
+            this.cb3.Name = "cb3";
+            this.cb3.Size = new System.Drawing.Size(80, 17);
+            this.cb3.TabIndex = 52;
+            this.cb3.Text = "checkBox3";
+            this.cb3.UseVisualStyleBackColor = true;
+            // 
+            // cb2
+            // 
+            this.cb2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb2.AutoSize = true;
+            this.cb2.Location = new System.Drawing.Point(135, 82);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(80, 17);
+            this.cb2.TabIndex = 51;
+            this.cb2.Text = "checkBox2";
+            this.cb2.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblTitle.Location = new System.Drawing.Point(181, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(196, 32);
+            this.lblTitle.TabIndex = 50;
+            this.lblTitle.Text = "WILD FINDER";
+            // 
+            // AnimalsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 418);
+            this.Controls.Add(this.cbHabitat);
+            this.Controls.Add(this.btnConsult);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.lbConsult);
+            this.Controls.Add(this.cb3);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "AnimalsForm";
+            this.Text = "Wild Finder";
+            this.Load += new System.EventHandler(this.AnimalsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbHabitat;
+        private System.Windows.Forms.Button btnConsult;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.ListBox lbConsult;
+        private System.Windows.Forms.CheckBox cb3;
+        private System.Windows.Forms.CheckBox cb2;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ WildFinder/WildFinder/AnimalsForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SbsSW.SwiPlCs;
+
+namespace WildFinder
+{
+    public partial class AnimalsForm : Form
+    {
+        public AnimalsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AnimalsForm_Load(object sender, EventArgs e)
+        {
+            Environment.SetEnvironmentVariable("Path", @"C:\\Program Files (x86)\\swipl\\bin");
+            String[] p = { "-q", "-f", @"Animals.pl" };
+            PlEngine.Initialize(p);
+        }
+
+        private void btnConsult_Click(object sender, EventArgs e)
+        {
+            var value = tbInput.Text;
+            lbConsult.Items.Clear();
+            var load = new PlQuery("load('animals.bd')");
+            try
+            {
+                load.NextSolution();
+
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.StackTrace);
+            }
+
+            if (cbHabitat.Checked)
+            {
+                var consulta1 = new PlQuery("lives_in(" + value + ",Animal)");
+                foreach (var z in consulta1.SolutionVariables)
+                    lbConsult.Items.Add(z["Animal"].ToString());
+            }
+        }
+    }
+}
